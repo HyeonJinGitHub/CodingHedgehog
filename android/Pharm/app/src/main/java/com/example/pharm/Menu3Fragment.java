2a0c74a5c4616b38b137eb9ff0222a3aaef1476b
YangSSo51 <48993188+YangSSo51@users.noreply.github.com>
2020-01-09 00:56:40 +0900

    add android

---
+++ android/Pharm/app/src/main/java/com/example/pharm/Menu3Fragment.java
@@ -0,0 +1,20 @@
+package com.example.pharm;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Menu3Fragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_menu3, container, false);
+    }
+}
+
